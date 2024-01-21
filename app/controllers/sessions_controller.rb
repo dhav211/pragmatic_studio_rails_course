8092a315e926a0255d9a6e566eb1c9e1ab9dac34
dhav211 <dhav211@gmail.com>
2024-01-21 11:03:21 -0800

    Add user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    fail
+    user = User.find_by username: params[:username]
+    if !user.nil? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}"
+    else
+      flash.now[:alert] = 'Incorrect username/password'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
